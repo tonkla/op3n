43e8394bdd9f516327ad1839c2db2f7464948657
Surakarn Samkaew <tonkla@gmail.com>
2017-09-12 23:03:45 +0700

    Use Gin instead of Chi

--- api/server.go
+++ api/server.go
@@ -1,46 +1,39 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 
-	"github.com/go-chi/chi"
+	"github.com/gin-gonic/gin"
 	truefx "github.com/tonkla/gotruefx"
 )
 
 func main() {
-	r := chi.NewRouter()
-
-	r.Get("/", home)
-	r.Route("/forex", func(r chi.Router) {
-		r.Get("/", getForexAll)
-		r.Get("/{symbol}", getForexBySymbol)
-	})
-
-	http.ListenAndServe(":3000", r)
+	r := gin.Default()
+	r.GET("/", home)
+	r.GET("/forex", getForexAll)
+	r.GET("/forex/:symbol", getForexBySymbol)
+	r.Run(":3000")
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Welcome"))
+func home(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "Please visit https://www.op3n.ga for document."})
 }
 
-func getForexAll(w http.ResponseWriter, r *http.Request) {
+func getForexAll(c *gin.Context) {
 	result := truefx.NewFeed().Get()
-	w.Header().Set("Content-Type", "application/json")
 	if len(result) > 0 {
-		json.NewEncoder(w).Encode(result)
+		c.JSON(http.StatusOK, result)
 	} else {
-		json.NewEncoder(w).Encode([]string{})
+		c.JSON(http.StatusNotFound, []truefx.Tick{})
 	}
 }
 
-func getForexBySymbol(w http.ResponseWriter, r *http.Request) {
-	symbol := chi.URLParam(r, "symbol")
+func getForexBySymbol(c *gin.Context) {
+	symbol := c.Param("symbol")
 	result := truefx.NewFeed().GetBySymbol(symbol)
-	w.Header().Set("Content-Type", "application/json")
 	if len(result) > 0 {
-		json.NewEncoder(w).Encode(result)
+		c.JSON(http.StatusOK, result)
 	} else {
-		json.NewEncoder(w).Encode([]string{})
+		c.JSON(http.StatusNotFound, []truefx.Tick{})
 	}
 }
