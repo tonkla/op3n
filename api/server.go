8cebf1b3641fce81171c8956d3e8aab6e5fc429b
Surakarn Samkaew <tonkla@gmail.com>
2017-09-29 14:06:42 +0700

    Fix conflicted route paths

--- api/server.go
+++ api/server.go
@@ -24,8 +24,8 @@ func main() {
 	r.GET("/thailand-provinces/:province/districts/:district", province.GetDistrict)
 	r.GET("/thailand-provinces/:province/districts/:district/subdistricts", province.GetAllSubdistricts)
 	r.GET("/thailand-provinces/:province/districts/:district/subdistricts/:subdistrict", province.GetSubdistrict)
-	r.GET("/thailand-provinces/postcodes", province.GetPostcodes)
-	r.GET("/thailand-provinces/postcodes/:postcode", province.GetByPostcode)
+	r.GET("/postcodes", province.GetPostcodes)
+	r.GET("/postcodes/:postcode", province.GetByPostcode)
 
 	r.Run(":3000")
 }
