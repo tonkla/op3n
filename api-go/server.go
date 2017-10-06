5177bd3f2fc4d75de67d185b2de66b18eaa7bc66
Surakarn Samkaew <tonkla@gmail.com>
2017-10-06 22:15:36 +0700

    Change routes

--- api-go/server.go
+++ api-go/server.go
@@ -18,14 +18,14 @@ func main() {
 	r.GET("/forex", forex.GetAll)
 	r.GET("/forex/:symbol", forex.GetBySymbol)
 
-	r.GET("/thailand-provinces", province.GetAllProvinces)
-	r.GET("/thailand-provinces/:province", province.GetProvince)
-	r.GET("/thailand-provinces/:province/districts", province.GetAllDistricts)
-	r.GET("/thailand-provinces/:province/districts/:district", province.GetDistrict)
-	r.GET("/thailand-provinces/:province/districts/:district/subdistricts", province.GetAllSubdistricts)
-	r.GET("/thailand-provinces/:province/districts/:district/subdistricts/:subdistrict", province.GetSubdistrict)
-	r.GET("/thailand-postcodes", province.GetPostcodes)
-	r.GET("/thailand-postcodes/:postcode", province.GetByPostcode)
+	r.GET("/thailand/provinces", province.GetAllProvinces)
+	r.GET("/thailand/provinces/:province", province.GetProvince)
+	r.GET("/thailand/provinces/:province/districts", province.GetAllDistricts)
+	r.GET("/thailand/provinces/:province/districts/:district", province.GetDistrict)
+	r.GET("/thailand/provinces/:province/districts/:district/subdistricts", province.GetAllSubdistricts)
+	r.GET("/thailand/provinces/:province/districts/:district/subdistricts/:subdistrict", province.GetSubdistrict)
+	r.GET("/thailand/postcodes", province.GetPostcodes)
+	r.GET("/thailand/postcodes/:postcode", province.GetByPostcode)
 
 	r.Run(":3000")
 }
